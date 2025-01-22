c0e5cd5c6fba0e2b309d346c7719c7be9986b20c
sackoba <badrasacko11@gmail.com>
2025-01-22 16:36:25 +0000

    add repositories

---
+++ lib/repositories/OtherCarRepository/i_other_car_repository.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+
+import '../../error/app_error.dart';
+import '../../models/gare/gare.dart';
+import '../../models/itineraire_gare/itineraire_gare.dart';
+
+abstract class IOtherCarRepository {
+  Future<Either<AppError, List<Gare>>> getAllGares();
+  Future<Either<AppError, List<ItineraireGare>>> getAllItinerary();
+  Future<Either<AppError, bool>> addAllGares();
+}
