97967a1999f9f8c11f44fc0fc6849b37db7c5313
sackoba <badrasacko11@gmail.com>
2025-01-25 13:45:00 +0000

    add header on table_template_view

--- lib/views/widgets/table_template_view .dart
+++ lib/views/widgets/table_template_view .dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mobilitydashboard/core/extensions/context_extensions.dart';
 import 'package:pluto_grid/pluto_grid.dart';
 
+import '../../core/assets/assets.gen.dart';
 import '../../responsive.dart';
 
 class TableTemplateView extends StatelessWidget {
@@ -136,7 +137,7 @@ class TableTemplateView extends StatelessWidget {
         },
       ),
     ];
-    late final PlutoGridStateManager stateManager;
+    // late final PlutoGridStateManager stateManager;
 
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 20.sp, vertical: 15.sp),
@@ -184,33 +185,113 @@ class TableTemplateView extends StatelessWidget {
                   ),
                 ),
               if (Responsive.isMobile(context))
-                IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+                IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+              PopupMenuButton(
+                tooltip: 'Menu',
+                icon: Assets.svg.menuVertical1.svg(
+                    color: context.colors.textColor,
+                    height: 30.sp,
+                    width: 30.sp),
+                itemBuilder: (context) => <PopupMenuEntry>[
+                  PopupMenuItem(
+                    child: const Text(
+                      "Print selected rows",
+                    ),
+                    onTap: () {},
+                  ),
+                  const PopupMenuDivider(),
+                  PopupMenuItem(
+                    child: const Text("Remove first row"),
+                    onTap: () {},
+                  ),
+                ],
+              ),
             ],
           ),
           context.gaps.small,
           Expanded(
             child: PlutoGrid(
+              createHeader: (stateManager) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: PopupMenuButton(
+                        tooltip: 'filtre',
+                        icon: Assets.svg.filter.svg(
+                            color: context.colors.textColor,
+                            height: 30.sp,
+                            width: 30.sp),
+                        itemBuilder: (context) => <PopupMenuEntry>[
+                          PopupMenuItem(
+                            child: const Text(
+                              "Print selected rows",
+                            ),
+                            onTap: () {},
+                          ),
+                          const PopupMenuDivider(),
+                          PopupMenuItem(
+                            child: const Text("appendNewRows"),
+                            onTap: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: IconButton(
+                        tooltip: 'Ajouter',
+                        onPressed: () {
+                          stateManager.appendRows([
+                            PlutoRow(
+                              cells: {
+                                'id': PlutoCell(value: 'user1 test'),
+                                'name': PlutoCell(value: 'Mike'),
+                                'age': PlutoCell(value: 20),
+                                'role': PlutoCell(value: 'Programmer'),
+                                'joined': PlutoCell(value: '2021-01-01'),
+                                'working_time': PlutoCell(value: '09:00'),
+                                'salary': PlutoCell(value: 300),
+                              },
+                            ),
+                          ]);
+                        },
+                        icon: Assets.svg.addRow.svg(
+                            color: context.colors.textColor,
+                            height: 35.sp,
+                            width: 35.sp),
+                      ),
+                    ),
+                  ],
+                );
+              },
               columns: columns,
               rows: rows,
               onLoaded: (PlutoGridOnLoadedEvent event) {
-                stateManager = event.stateManager;
+                // stateManager = event.stateManager;
               },
               onChanged: (PlutoGridOnChangedEvent event) {
                 print(event);
               },
+              // onRowChecked: (event) {
+              //   event.row!.setChecked(true);
+              //   print(event.row!.toJson());
+              // },
               configuration: PlutoGridConfiguration(
-                  columnFilter: PlutoGridColumnFilterConfig(),
                   style: PlutoGridStyleConfig.dark(
-                    enableColumnBorderVertical: false,
-                    enableCellBorderVertical: false,
-                    enableRowColorAnimation: true,
-                    activatedColor: context.colors.lightPrimary,
-                    rowColor: context.colors.secondary,
-                    gridBorderColor: context.colors.secondary,
-                    gridBorderRadius: BorderRadius.circular(8.sp),
-                    borderColor: context.colors.textColor,
-                    gridBackgroundColor: context.colors.secondary,
-                  )),
+                enableColumnBorderVertical: false,
+                enableCellBorderVertical: false,
+                enableRowColorAnimation: true,
+                cellColorInEditState: context.colors.textColor,
+                checkedColor: context.colors.lightPrimary,
+                activatedColor: context.colors.lightPrimary,
+                rowColor: context.colors.secondary,
+                gridBorderColor: context.colors.secondary,
+                gridBorderRadius: BorderRadius.circular(8.sp),
+                borderColor: context.colors.textColor,
+                gridBackgroundColor: context.colors.secondary,
+              )),
             ),
           ),
         ],
